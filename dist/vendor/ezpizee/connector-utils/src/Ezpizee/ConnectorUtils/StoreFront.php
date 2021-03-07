4023880d465305a3672e72db6223bcd256ee34cd
Sothea Nim <sothea.nim@ezpizee.com>
2021-03-07 18:14:20 +0700

    updated

---
+++ dist/vendor/ezpizee/connector-utils/src/Ezpizee/ConnectorUtils/StoreFront.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ezpizee\ConnectorUtils;
+
+class StoreFront
+{
+    const EZPZ_KEY_CSRF_TOKEN = "CSRF-Token";
+    const EZPZ_KEY_CART_ID = "ezpz_cart_id";
+    const EZPZ_KEY_ORDER_ID = "ezpz_order_id";
+    const EZPZ_ATTR = "data-ezpz";
+    const EZPZ_ATTR_PRICE_FOR = "data-ezpz-price-for";
+    const EZPZ_ATTR_MERCHANT = "data-ezpz-merchant";
+    const EZPZ_HBS_CART = "cart";
+}
